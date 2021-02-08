af31c7effe8a91374cae39c0538a254a76597c06
scanoss-qg <78024084+scanoss-qg@users.noreply.github.com>
2021-02-08 14:51:45 -0300

    Add files via upload

---
+++ inc/mz_mine.h
@@ -0,0 +1,9 @@
+#ifndef __MZ_MINE_H
+#define __MZ_MINE_H
+
+
+void mz_mine_copyright(struct mz_job *job);
+void mz_mine_quality(struct mz_job *job);
+void mz_mine_license(struct mz_job *job);
+void mz_mine_crypto(struct mz_job *job);
+#endif
